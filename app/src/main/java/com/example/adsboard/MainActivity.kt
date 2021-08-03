fc0a8e00f7219243998317c03fbf8ff6d708e431
Talyat <talyatcsdok@mail.ru>
2021-08-03 13:05:59 +0300

    Create Toolbar, tied DrawerLayout to Activity, create toogle

--- app/src/main/java/com/example/adsboard/MainActivity.kt
+++ app/src/main/java/com/example/adsboard/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.adsboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.adsboard.databinding.ActivityMainBinding
+import com.example.adsboard.databinding.ToolbarBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+
+    private fun init() {
+        val toolbar: ToolbarBinding = ToolbarBinding.inflate(layoutInflater)
+        val toogle = ActionBarDrawerToggle(this, binding.drawerLayout, toolbar.toolbar, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+    }
+
 }
